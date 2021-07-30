cf2f3a3568e6ba1af59f7aa0ab4e590ef3aacaa1
Leontiy Shcherbyna <leontiy@Leontiys-MBP.fritz.box>
2021-07-30 12:51:35 +0200

    Haben gerade angefangen einen Controller zu bauen

---
+++ backend/src/main/java/de/neuefische/todobackend/controller/TodoController.java
@@ -0,0 +1,19 @@
+package de.neuefische.todobackend.controller;
+
+import de.neuefische.todobackend.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/todo")
+public class TodoController {
+
+    private final TodoService todoService;
+
+    @Autowired
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+
+    }
+}
--- backend/src/main/java/de/neuefische/todobackend/repo/TodoRepo.java
+++ backend/src/main/java/de/neuefische/todobackend/repo/TodoRepo.java
@@ -1,9 +1,11 @@
 package de.neuefische.todobackend.repo;
 
 import de.neuefische.todobackend.model.Todo;
+import org.springframework.stereotype.Repository;
 
 
 import java.util.List;
+@Repository
 
 public class TodoRepo {
     private List<Todo> todoList;
